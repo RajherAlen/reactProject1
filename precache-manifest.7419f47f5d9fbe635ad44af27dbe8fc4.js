0eca7531c5260e3e41bd42ec397c70e101a3c6c3
Alen <alen.rajher@gmail.com>
2020-05-08 23:16:59 +0200

    Updates

---
+++ precache-manifest.7419f47f5d9fbe635ad44af27dbe8fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f188e95f29c2a046adfd8eb5c9447041",
+    "url": "/reactProject1/index.html"
+  },
+  {
+    "revision": "0151af688ee1622e7a8c",
+    "url": "/reactProject1/static/css/main.8729d157.chunk.css"
+  },
+  {
+    "revision": "9e2fc4fa2b764dd8ffa2",
+    "url": "/reactProject1/static/js/2.87428bf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactProject1/static/js/2.87428bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0151af688ee1622e7a8c",
+    "url": "/reactProject1/static/js/main.57761061.chunk.js"
+  },
+  {
+    "revision": "71d791c74c544f07ea2d",
+    "url": "/reactProject1/static/js/runtime-main.af6989a4.js"
+  }
+]);
